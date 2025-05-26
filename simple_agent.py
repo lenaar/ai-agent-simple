29d7d57b193f9b59a960c8c84a37510e730c1e23
Elena <rahimova.elena@gmail.com>
2025-05-26 20:48:19 +0200

    feat: implement SimpleAgent class with message history

--- simple_agent.py
+++ simple_agent.py
@@ -2,7 +2,7 @@ import os
 from openai import OpenAI
 from dotenv import load_dotenv
 
-load_dotenv()   
+load_dotenv()
 
 openai = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
@@ -16,8 +16,27 @@ response = openai.chat.completions.create(
     ]
 )
 
-print(response.choices[0].message.content)
+#print(response.choices[0].message.content)
 
+# add types to the class
+class SimpleAgent:
+    def __init__(self, model_name: str = "gpt-4.1-nano", system_prompt: str = "You are an intelligent, kind and helpful assistant."):
+        self.model_name = model_name
+        self.system_prompt = system_prompt
+        self.messages = []
+        if system_prompt:
+            self.messages.append({"role": "system", "content": system_prompt})
 
-
-
+    def __call__(self, user_message: str):
+        self.messages.append({"role": "user", "content": user_message})
+        assistant_response = self.execute()
+        self.messages.append({"role": "assistant", "content": assistant_response})
+        return assistant_response
+    
+    def execute(self) -> str:
+        assistant_response = openai.chat.completions.create(
+            model=self.model_name,
+            temperature=0.0, # 0.0 - deterministic, 1.0 - random (creative)
+            messages=self.messages
+        )
+        return assistant_response.choices[0].message.content
