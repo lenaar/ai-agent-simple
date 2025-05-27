e61d175801b89018cbde39628f98135f57afbfb7
Elena <rahimova.elena@gmail.com>
2025-05-27 22:59:59 +0200

    feat: create BasicNodeTool and route_tool

--- langgraph_agent.py
+++ langgraph_agent.py
@@ -3,16 +3,16 @@ from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langgraph.graph.message import add_messages
 from langgraph.graph import StateGraph, END
-from langgraph_agent_tools import tools
-from typing import Annotated, TypedDict
+from langgraph_agent_tools import tools, BasicToolNode
+from typing import Annotated, TypedDict, Literal
 
 load_dotenv()
 
 openai = ChatOpenAI(api_key=os.getenv("OPENAI_API_KEY"), model_name="gpt-4.1-nano", temperature=0.0)
 model_with_tools = openai.bind_tools(tools)
 
-response = model_with_tools.invoke([{"role": "user", "content": "What is the capital of France?"}])
-print(response)
+# response = model_with_tools.invoke([{"role": "user", "content": "What is the capital of France?"}])
+# print(response)
 
 # state to keep messages
 class MessagesState(TypedDict):
@@ -30,11 +30,25 @@ def simple_agent_langgraph(state: MessagesState):
 
     return {"messages": [model_with_tools.invoke(state["messages"])]}
 
+def route_tools(state: MessagesState) -> Literal["tools", END]:
+    """Use the conditional node to route the messages to the correct tool. Otherwise, end the graph."""
+    if isinstance(state, list):
+        ai_message = state[-1]
+    elif messages := state.get("messages", []):
+        ai_message = messages[-1]
+    else:
+        raise ValueError("No messages found in state")
+    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
+        return "tools"
+    return END
+    
 
 def build_graph_agent():
     # Initialize the graph
     graph = StateGraph(MessagesState)
-    
+    tool_node = BasicToolNode(tools=tools)
+    graph.add_conditional_edges("agent", route_tools, {"tools": "tools", END: END})
+    graph.add_node("tools", tool_node)
     # Add the agent node
     graph.add_node("agent", simple_agent_langgraph)
     
@@ -42,7 +56,7 @@ def build_graph_agent():
     graph.set_entry_point("agent")
     
     # Set the end point
-    graph.add_edge("agent", END)
+    # graph.add_edge("agent", END)
     
     # Compile the graph
     return graph.compile()
