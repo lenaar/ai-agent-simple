cf4ec54f31ef681759472e9b77ca8c8837cbec34
Elena <rahimova.elena@gmail.com>
2025-05-27 21:22:34 +0200

    clean: old comment

--- langgraph_agent_tools.py
+++ langgraph_agent_tools.py
@@ -5,14 +5,6 @@ from langchain_community.tools.tavily_search import TavilySearchResults
 
 load_dotenv()
 
-# def tool_tavily_search(query: str) -> str:
-#     """Search the internet for information about a topic. Use this when you need real-time or factual information.
-    
-#     Args:
-#         query (str): The search query to look up
-#     """
-#     tavily = TavilySearchResults(max_results=5)
-#     return tavily.invoke(query)
 
 
 tool_tavily_search = TavilySearchResults(max_results=5)
