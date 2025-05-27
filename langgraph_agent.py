b64d96a0aa4b002faa5dda2a4777fc454430324e
Elena <rahimova.elena@gmail.com>
2025-05-27 23:22:38 +0200

    fix: use prebuild tool node and tool conditions

--- langgraph_agent.py
+++ langgraph_agent.py
@@ -3,12 +3,14 @@ from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langgraph.graph.message import add_messages
 from langgraph.graph import StateGraph, END
-from langgraph_agent_tools import tools, BasicToolNode
-from typing import Annotated, TypedDict, Literal
+from langgraph_agent_tools import tools
+from typing import Annotated, TypedDict
+from langgraph.prebuilt import ToolNode, tools_condition
 
 load_dotenv()
 
 openai = ChatOpenAI(api_key=os.getenv("OPENAI_API_KEY"), model_name="gpt-4.1-nano", temperature=0.0)
+# to invoke the model with tools use more complex query, like "find a hotel near Manhatten in New York"
 model_with_tools = openai.bind_tools(tools)
 
 # response = model_with_tools.invoke([{"role": "user", "content": "What is the capital of France?"}])
@@ -22,29 +24,13 @@ def simple_agent_langgraph(state: MessagesState):
     """
     Example: state = {
     "messages": [
-        {"role": "user", "content": "What's 2+2?"}
+        {"role": "user", "content": "find a hotel near Manhatten in New York"}
         ]
     }
     """
     print(state["messages"])
 
     return {"messages": [model_with_tools.invoke(state["messages"])]}
-
-def route_tools(state: MessagesState) -> Literal["tools", END]:
-    """Use the conditional node to route the messages to the correct tool. Otherwise, end the graph."""
-    if isinstance(state, list):
-        print("state is a list")
-        ai_message = state[-1]
-    elif messages := state.get("messages", []):
-        print("state is a dict")
-        ai_message = messages[-1]
-    else:
-        raise ValueError("No messages found in state")
-    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
-        print("ai_message has tool_calls")
-        return "tools"
-    print("ai_message has no tool_calls")
-    return END
     
 
 def build_graph_agent():
@@ -55,13 +41,9 @@ def build_graph_agent():
     # Set the entry point - where the graph begins
     graph.set_entry_point("agent")
 
-    tool_node = BasicToolNode(tools=tools)
-    graph.add_conditional_edges("agent", route_tools, {"tools": "tools", END: END})
+    tool_node = ToolNode(tools=tools)
+    graph.add_conditional_edges("agent", tools_condition)
     graph.add_node("tools", tool_node)
-    # Add the agent node
-    
-    # Set the end point
-    # graph.add_edge("agent", END)
     
     # Compile the graph
     return graph.compile()
--- langgraph_agent_tools.py
+++ langgraph_agent_tools.py
@@ -1,9 +1,5 @@
-import os
-from typing import Optional
 from dotenv import load_dotenv
 from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain_core.messages import ToolMessage
-import json
 
 load_dotenv()
 
@@ -17,28 +13,4 @@ tools = [tool_tavily_search]
 # res = tavily.invoke("What is the capital of France?")
 # print(res)
 
-class BasicToolNode:
-    """A node that runs the tools requested in the last AIMessage."""
-
-    def __init__(self, tools: list) -> None:
-        self.tools_by_name = {tool.name: tool for tool in tools}
-
-    def __call__(self, inputs: dict):
-        if messages := inputs.get("messages", []):
-            message = messages[-1]
-        else:
-            raise ValueError("No message found in input")
-        outputs = []
-        for tool_call in message.tool_calls:
-            tool_result = self.tools_by_name[tool_call["name"]].invoke(
-                tool_call["args"]
-            )
-            outputs.append(
-                ToolMessage(
-                    content=json.dumps(tool_result),
-                    name=tool_call["name"],
-                    tool_call_id=tool_call["id"],
-                )
-            )
-        return {"messages": outputs}
 
